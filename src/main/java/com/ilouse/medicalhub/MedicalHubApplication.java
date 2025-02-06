3ab5e8b29340af675275d01c9cd01ec7efb49800
ilyas-ben <ilyasben313@gmail.com>
2025-02-06 01:21:10 +0100

    first commit

---
+++ src/main/java/com/ilouse/medicalhub/MedicalHubApplication.java
@@ -0,0 +1,13 @@
+package com.ilouse.medicalhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalHubApplication.class, args);
+	}
+
+}
