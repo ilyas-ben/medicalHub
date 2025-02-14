4c153cb731e0a4b9bee9d9b8a4e54b29e69f0963
ilyas-ben <ilyasben313@gmail.com>
2025-02-14 09:19:56 +0100

    added @Data lombok annotation

--- backend/src/main/java/com/ilouse/medicalhub/model/Appointment.java
+++ backend/src/main/java/com/ilouse/medicalhub/model/Appointment.java
@@ -2,10 +2,12 @@ package com.ilouse.medicalhub.model;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 public class Appointment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,59 +22,4 @@ public class Appointment {
 
     @Column
     Boolean status;
-
-
-
-    public Appointment() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Patient getPatient() {
-        return patient;
-    }
-
-    public void setPatient(Patient patient) {
-        this.patient = patient;
-    }
-
-    public Boolean getStatus() {
-        return status;
-    }
-
-    public void setStatus(Boolean status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "Appointment{" +
-                "id=" + id +
-                ", patient=" + patient +
-                ", dateTime=" + dateTime +
-                ", status=" + status +
-                '}';
-    }
-
-    public Appointment(Long id, Patient patient, LocalDateTime dateTime, Boolean status) {
-        this.id = id;
-        this.patient = patient;
-        this.dateTime = dateTime;
-        this.status = status;
-    }
-
 }
--- backend/src/main/java/com/ilouse/medicalhub/model/Patient.java
+++ backend/src/main/java/com/ilouse/medicalhub/model/Patient.java
@@ -3,6 +3,8 @@ package com.ilouse.medicalhub.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Data
@@ -20,55 +22,4 @@ public class Patient {
 
     @Column
     String socialSecurityNumber;
-
-    public Patient() {}
-
-    @Override
-    public String toString() {
-        return "Patient{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", patientNumber=" + patientNumber +
-                ", socialSecurityNumber='" + socialSecurityNumber + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getPatientNumber() {
-        return patientNumber;
-    }
-
-    public void setPatientNumber(Integer patientNumber) {
-        this.patientNumber = patientNumber;
-    }
-
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
-
-    public void setSocialSecurityNumber(String socialSecurityNumber) {
-        this.socialSecurityNumber = socialSecurityNumber;
-    }
-
-    public Patient(Long id, String name, Integer patientNumber, String socialSecurityNumber) {
-        this.id = id;
-        this.name = name;
-        this.patientNumber = patientNumber;
-        this.socialSecurityNumber = socialSecurityNumber;
-    }
 }
