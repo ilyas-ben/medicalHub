3aa0139ee3ab1db1839d4f6f4b3b93167484c377
ilyas-ben <ilyasben313@gmail.com>
2025-02-27 22:07:40 +0000

    implementing add appointment in the form

--- frontend/src/app/components/appointment/appointments.component.ts
+++ frontend/src/app/components/appointment/appointments.component.ts
@@ -2,21 +2,38 @@ import { Component, OnInit, signal } from '@angular/core';
 import { AppointmentsService } from '../../services/appointment/appointment.service';
 import { Appointment } from '../../model/appointment.type';
 import { CommonModule } from '@angular/common';
+import { PatientService } from '../../services/patient/patient.service';
+import { Patient } from '../../model/patient.type';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-appointments',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.scss'],
 })
 export class AppointmentsComponent implements OnInit {
   appointments = signal<Array<Appointment>>([]);
+  patients = signal<Array<Patient>>([]);
+  newAppointmentForm: FormGroup;
 
-  constructor(private appointmentsService: AppointmentsService) {}
+  constructor(private appointmentsService: AppointmentsService, private fb: FormBuilder, private patientService: PatientService) {
+    this.newAppointmentForm = this.fb.group({
+      patientId: ['', Validators.required],
+      dateTime: ['', Validators.required],
+      status: [true, Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.setAppointments();
+    this.setPatients();
   }
 
   setAppointments(): void {
@@ -25,11 +42,19 @@ export class AppointmentsComponent implements OnInit {
     });
   }
 
-  /* save() {
-    const newAppointment: Appointment[] = [];
-    newAppointment.push(this.newAppointmentForm.value);
+  setPatients(): void {
+    this.patientService.findAll().subscribe((items: Patient[]) => {
+      this.patients.set(items);
+    });
+  }
+  save() {
+    const newAppointment: Appointment = this.newAppointmentForm.value;
 
-    this.appointmentService.save(newAppointment).subscribe(() => {
+    newAppointment.patient = { id: this.newAppointmentForm.value.patientId } as Patient; 
+
+    /* patient: , */
+
+    this.appointmentsService.save([{...this.newAppointmentForm.value}]).subscribe(() => {
       this.setAppointments();
       this.newAppointmentForm.reset();
 
@@ -42,7 +67,9 @@ export class AppointmentsComponent implements OnInit {
         }
       }
     });
-  } */
+
+    console.log(newAppointment);
+  }
 
   deleteById(idAppointmentToDelete: number): void {
     this.appointmentsService.deleteByID(idAppointmentToDelete).subscribe(() => {
